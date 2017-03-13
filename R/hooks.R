8faaf92224723dabe53b52248ca6cff16928bb47
Eduardo Flores <eduardo@enelmargen.org>
2017-03-13 12:29:21 -0600

    Update hooks.R

--- R/hooks.R
+++ R/hooks.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname){
-  packageStartupMessage("Biblioteca de Utensilios Generales para R (v0.1) - Topaz Soluciones Educativas - *** cargando: dplyr, inegiR, ggplot2, magrittr, foreign, memisc, RODBC")
+  packageStartupMessage("Biblioteca de Utensilios Generales para R (v0.2) - Topaz Soluciones Educativas - *** cargando: dplyr, inegiR, ggplot2, magrittr, foreign, memisc, RODBC")
   # variables a cargar
   token_inegi <<-"502ae69d-a835-8bd5-0e73-2173cc35c83d"
   token_inegi_denue <<-"a3c02469-d06d-421a-95e5-8401047c6786"
